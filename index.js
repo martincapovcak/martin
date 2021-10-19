917743eac23ddee18d4ba46576f028269fbc62a4
Martin <martin.capovcak@gmail.com>
2021-10-19 17:11:08 +0200

    coloring CLI

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
+const chalk = require('chalk');
+
+const log = console.log;
+
+const dim = chalk.dim;
+const italic = chalk.italic;
+const gitHubColor = chalk.hex(`#6cc644`).bold.inverse;
+const linkedInColor = chalk.hex(`#0077b5`).bold.inverse;
+
 
 welcome({
-    title: pkgJSON.name,
+    title: `Martin Capovcak`,
     tagLine: `Hey, nice to meet you!`,
     description: pkgJSON.description,
     version: pkgJSON.version,
@@ -13,13 +22,11 @@ welcome({
     clear: true
 });
 
-console.log(`
-Martin Capovcak
-
-Self driven full-stack web developer.
+log(`
+${italic(`Self driven full-stack web developer.
 Primarily focused on MERN (MongoDB, Express, React and Node) technologies.
-In passion with bee-keeping.
+In passion with bee-keeping.`)}
 
-📖 GitHub: https://github.com/martincapovcak
-
+${gitHubColor(` GitHub `)} ${dim(`https://github.com/martincapovcak`)}
+${linkedInColor(` LinkedIn `)} ${dim(`https://www.linkedin.com/in/martin-capovcak-3910b780`)}
 `);
