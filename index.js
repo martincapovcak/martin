229e88a119f69ed07d72ac450e32c878d8a8eeea
Martin <martin.capovcak@gmail.com>
2021-10-19 15:31:13 +0200

    CLI welcome Header

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
 const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
 
-//Clears console
-clearConsole();
-
-console.log(`
-NAME: ${pkgJSON.name}
-VERSION: ${pkgJSON.version}
-DESCRIPTION: ${pkgJSON.description}
-`);
+welcome({
+    title: pkgJSON.name,
+    tagLine: `Hey, nice to meet you!`,
+    description: pkgJSON.description,
+    version: pkgJSON.version,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+    clear: true
+});
 
 console.log(`
 Martin Capovcak
