225980f5e8dab02fd81f31e1625ab325360c07d5
Martin <martin.capovcak@gmail.com>
2021-10-19 13:50:01 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+console.log(`
+Martin Capovcak
+
+Self driven full-stack web developer.
+Primarily focused on MERN (MongoDB, Express, React and Node) technologies.
+In passion with bee-keeping.
+
+📖 GitHub: https://github.com/martincapovcak
+
+`);
