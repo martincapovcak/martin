9520397b1cf658c3ba0399ea958882d07ef446c1
Martin <martin.capovcak@gmail.com>
2021-10-21 00:05:44 +0200

    refactoring data

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
 const alert = require('bee-alerts');
 const handleError = require('cli-handle-error');
 
 const init = require('./utils/init.js');
+const data = require('./utils/data.js');
 
-// color styling
-const dim = chalk.dim;
-const italic = chalk.italic;
-const gitHubColor = chalk.hex(`#6cc644`).bold.inverse;
-const linkedInColor = chalk.hex(`#0077b5`).bold.inverse;
 
 (() => {
     init();
-
-    console.log(
-        `${italic(`Self driven full-stack web developer. 
-Primarily focused on MERN (MongoDB, Express, React and NodeJS) technologies.
-In passion with bee-keeping.`)}
-
-${gitHubColor(` GitHub `)} ${dim(`https://github.com/martincapovcak`)}
-${linkedInColor(` LinkedIn `)} ${dim(`https://www.linkedin.com/in/martin-capovcak-3910b780`)}
-`);
-
-    alert({
-        type: `info`,
-        msg: `Custom CLIs are fun.`,
-        name: `WoHooo`
-    });
-
-    const err = new Error(`Blog API`);
-    handleError(`Blog API is down!`, err, true, false);
+    console.log(data.bio);
+    console.log(data.social);
+    alert({type: `info`,msg: data.info ,name: `WoHooo`});
 })();
---
+++ utils/data.js
@@ -0,0 +1,26 @@
+const chalk = require('chalk');
+
+// color styling
+const dim = chalk.dim;
+const italic = chalk.italic;
+const gitHubColor = chalk.hex(`#6cc644`).bold.inverse;
+const linkedInColor = chalk.hex(`#0077b5`).bold.inverse;
+
+const social = `
+${gitHubColor(` GitHub `)} ${dim(`https://github.com/martincapovcak`)}
+${linkedInColor(` LinkedIn `)} ${dim(`https://www.linkedin.com/in/martin-capovcak-3910b780`)}
+`;
+
+const bio = `
+${italic(`Self driven full-stack web developer. 
+Primarily focused on MERN (MongoDB, Express, React and NodeJS) technologies.
+In passion with bee-keeping.`)}
+`;
+
+const info = `Custom CLIs are fun.`;
+
+module.exports = {
+    bio,
+    social,
+    info
+};
