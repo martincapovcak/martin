e28e5dcd14cf7e0980809211b2b66fcd6e0e1a70
Martin <martin.capovcak@gmail.com>
2021-10-20 21:05:28 +0200

    Check node version feature

--- index.js
+++ index.js
@@ -2,22 +2,14 @@
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
 const chalk = require('chalk');
-
-const log = console.log;
+const alert = require('bee-alerts');
+const checkNode = require('cli-check-node');
 
 // color styling
 const dim = chalk.dim;
 const italic = chalk.italic;
 const gitHubColor = chalk.hex(`#6cc644`).bold.inverse;
 const linkedInColor = chalk.hex(`#0077b5`).bold.inverse;
-
-// alerts
-const sym = require('log-symbols');
-const success = chalk.green;
-const info = chalk.blue;
-const warning = chalk.keyword('orange');
-const error = chalk.red.bold; 
-
 
 welcome({
     title: `Martin Capovcak`,
@@ -30,19 +22,13 @@ welcome({
     clear: true
 });
 
-log(`
+checkNode('12');
+
+console.log(`
 ${italic(`Self driven full-stack web developer.
 Primarily focused on MERN (MongoDB, Express, React and Node) technologies.
 In passion with bee-keeping.`)}
 
 ${gitHubColor(` GitHub `)} ${dim(`https://github.com/martincapovcak`)}
 ${linkedInColor(` LinkedIn `)} ${dim(`https://www.linkedin.com/in/martin-capovcak-3910b780`)}
-`);
-
-
-log(`
-${sym.success} ${success(` SUCCESS `)} Thanks for checking my CLI.
-${sym.info} ${info(` INFO `)} CLIs are lot of fun.
-${sym.warning} ${warning(` WARNING `)} Hey, stay safe and healthy!
-${sym.error} ${error(` ERROR `)} I'am on vacation. Contact me next week.
 `);
